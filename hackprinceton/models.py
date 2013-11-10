b9e34c1d053960e229d0759828e43349d172b34e
Green Choi <ghchoi@princeton.edu>
2013-11-09 20:01:14 -0500

    initial commit

---
+++ hackprinceton/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+    )
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+
+
+class Page(Base):
+    """ The SQLAlchemy declarative model class for a Page object. """
+    __tablename__ = 'pages'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text, unique=True)
+    data = Column(Text)
+
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+
+
+##Index('my_index', MyModel.name, unique=True, mysql_length=255)
---
+++ hackprinceton/scripts/initializedb.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import transaction
+
+from sqlalchemy import engine_from_config
+
+from pyramid.paster import (
+    get_appsettings,
+    setup_logging,
+    )
+
+from pyramid.scripts.common import parse_vars
+
+from ..models import (
+    DBSession,
+    Page,
+    Base,
+    )
+
+
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri> [var=value]\n'
+          '(example: "%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
+
+
+def main(argv=sys.argv):
+    if len(argv) < 2:
+        usage(argv)
+    config_uri = argv[1]
+    options = parse_vars(argv[2:])
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri, options=options)
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    with transaction.manager:
+        page1 = Page(name="page11111", data="allan is a butthole 111111")
+        page2 = Page(name='page22222', data="allan is bad man 222222")
+        DBSession.add(page1)
+        DBSession.add(page2)
